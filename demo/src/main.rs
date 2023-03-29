59e0ee4ff4c01ea1e9c5675a1065c8d129f54f37
cisco <codingmaster.wang@outlook.com>
2023-03-29 11:02:30 +0800

    流程控制

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,8 +1,1 @@
-fn main() {
-    let n = 123456;
-    let description = match true { // 这里的true可以是其他表达式（比如带有n的函数）
-        true => "even",
-        false => "odd",
-    };
-    println!("{} is {}", n, description)
-}
+fn main() {}
