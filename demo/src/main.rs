f2d62cee321d2ac752dfc5b740567c8352dba617
cisco <codingmaster.wang@outlook.com>
2023-03-31 16:11:18 +0800

    字符，字符串

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,21 +1,21 @@
-use std::ops::{Add};
-// <1>
-use std::time::{Duration};                       // <2>
-
-fn add<T: Add<Output=T>>(i: T, j: T) -> T {    // <3>
-    i + j
-}
-
 fn main() {
-    let floats = add(1.2, 3.4);      // <4>
-    let ints = add(10, 20);          // <5>
-    let durations = add(             // <6>
-                                     Duration::new(5, 0),           // <6>
-                                     Duration::new(10, 0),           // <6>
-    );
+    let search_term = "picture";
+    let quote = "\
+Every face, every shop, bedroom window, public-house, and
+dark square is a picture feverishly turned--in search of what?
+It is the same with books.
+What do we seek through millions of pages?"; // 要表示一个多行字符串，并不需要特殊的语法。反斜线（\）字符会使编译器忽略掉紧跟着的换行符
 
-    println!("{}", floats);
-    println!("{}", ints);
-    println!("{:?}", durations);    // <7>
+    for line in quote.lines() { // lines()返回一个引用的迭代器，每次迭代都返回此文本中的一行。Rust会按照每种操作系统的约定来表示相应的换行符。quote.lines()表示按行迭代，并且是以跨平台的方式来实现的。
+        if line.contains(search_term) { // line.contains() 执行文本的查找
+            println!("{}", line);
+        }
+    }
+
+    for (i, line) in quote.lines().enumerate() { // lines()返回一个引用的迭代器，可以和enumerate形成链式调用。
+        if line.contains(search_term) {
+            println!("{}: {}", i + 1, line);
+        }
+    }
 }
 
