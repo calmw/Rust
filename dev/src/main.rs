2d1880fffade86089aee00b64999f7bd3d8c2704
cisco <codingmaster.wang@outlook.com>
2023-04-01 17:51:42 +0800

    枚举体

--- dev/src/main.rs
+++ dev/src/main.rs
@@ -1,7 +1,38 @@
-struct Hostname(String); // Hostname是新类型
-
-fn connect(host: Hostname) {// 使用类型系统来防范无效的用法。
-    println!("connected to {}", host.0) // 使用一个数字索引来访问其底层数据
+#[derive(Debug)]
+enum Event {
+    // 创建了包含三个事件的枚举体，其中最后一个值为无法识别的事件
+    Update,
+    Delete,
+    Unknown,
 }
 
-fn main() {}
+type Message = String;   // 定义新类型（起别名），给String一个更合适的名字，可以在这个create的上下文使用
+
+fn parse_log(line: &str) -> (Event, Message) {   // <4>
+    let parts: Vec<_> = line                       // <5>
+        .splitn(2, ' ')
+        .collect();                // <6>
+    if parts.len() == 1 {                          // <7>
+        return (Event::Unknown, String::from(line));
+    }
+
+    let event = parts[0];                // <8>
+    let rest = String::from(parts[1]);   // <8>
+
+    match event { // 当我们匹配到一个数据时，就返回结构化的数据
+        "UPDATE" | "update" => (Event::Update, rest),  // <9>
+        "DELETE" | "delete" => (Event::Delete, rest),  // <9>
+        _ => (Event::Unknown, String::from(line)),    // 不能识别这个事件类型的时候，就把整行数据返回
+    }
+}
+
+fn main() {
+    let log = "BEGIN Transaction XK342
+UPDATE 234:LS/32231 {\"price\": 31.00} -> {\"price\": 40.00}
+DELETE 342:LO/22111";
+
+    for line in log.lines() {
+        let parse_result = parse_log(line);
+        println!("{:?}", parse_result);
+    }
+}
