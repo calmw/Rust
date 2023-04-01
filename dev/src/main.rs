b9b111333a27af0be46e3ede8a3b93f6baa0aa86
cisco <codingmaster.wang@outlook.com>
2023-04-01 15:27:42 +0800

    复合数据类型

--- dev/src/main.rs
+++ dev/src/main.rs
@@ -1,6 +1,7 @@
-fn read(text: &mut String) -> () { // 显式的返回单元类型
-    *text = String::from("");
-}
+struct Hostname(String); // Hostname是新类型
 
+fn connect(host: Hostname) {// 使用类型系统来防范无效的用法。
+    println!("connected to {}", host.0) // 使用一个数字索引来访问其底层数据
+}
 
 fn main() {}
