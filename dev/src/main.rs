ccc13b40ae51420b2d1fe992f059cee0def5aa1c
cisco <codingmaster.wang@outlook.com>
2023-04-02 15:31:44 +0800

    所有权

--- dev/src/main.rs
+++ dev/src/main.rs
@@ -1,41 +1,21 @@
-#![allow(dead_code)]
-//! 表示当前语言项的文档，即编译器刚刚进入的哪个模块
-
-/// 用于注解其后紧跟着的语言项1
-/// 用于注解其后紧跟着的语言项2
-
-
-use std::fmt;
-use std::fmt::{Display};
-
-#[derive(Debug, PartialEq)]
-// 如果将整个枚举标记为公有的，则其变体也为公有的
-pub enum FileState {
-    Open,
-    Closed,
-}
-
 #[derive(Debug)]
-pub struct File {
-    pub name: String,
-    // 当第三方代码使用use来导入这个包时，File.data依然保持为私有的
-    data: Vec<u8>,
-    pub state: FileState,
+struct Demo {
+    a: i32,
 }
 
-impl File {
-    /// New files are assumed to be empty
-    /// ## new 方法注释
-    /// ```
-    /// let f = File::new("f1.txt")
-    /// ```
-    pub fn new(name: &str) -> File {
-        File {
-            name: String::from(name),
-            data: Vec::new(),
-            state: FileState::Closed,
-        }
-    }
+fn send(to: CubeSat, msg: Message) {
+    to.mailbox.messages.push(msg)
 }
 
-fn main() {}
+fn send(to: &mut CubeSat, msg: Message) {
+    to.mailbox.messages.push(msg)
+}
+
+fn use_value(_val: Demo) {}
+
+fn main() {
+    let a = Demo { a: 3 };
+    use_value(a);
+    println!("{:?}", a)
+}
+// 编译不通过
