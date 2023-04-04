425276a9d8b320841c251705fa3d8dde26d9ba90
cisco <codingmaster.wang@outlook.com>
2023-04-04 13:44:10 +0800

    整数

--- dev/src/main.rs
+++ dev/src/main.rs
@@ -1,15 +1,6 @@
+#[allow(arithmetic_overflow)] // arithmetic 算术。告诉编译器允许算数溢出. 这样的代码可以编译通过，但运行会出现panic
 fn main() {
-    let a: f32 = 42.42;
-    let frankentype: u32 = unsafe {  // 使用unsafe关键字并不表示此段代码具有内在的危险性，比如它不允许你绕过rust的借用检查器。在unsafe块允许的各种功能中，某些功能要比其他一些功能更加难以验证。就来说std::mem::transmute()函数就是rust语言中最不安全的功能之一，它已经没有任何类型的安全性可言。因此你要在使用它之前，请先研究有没有其他替代方案。
-        std::mem::transmute(a)  // std::mem::transmute()函数要求rust在不影响任何底层位数据的情况下，把一个f32直接解释成一个u32。
-    };
-
-    println!("{}", frankentype);              // <2>
-    println!("{:032b}", frankentype); // {:032b},在这里032的意思时"占用32，位数不足的在其左侧补零"，在其右边的b会调用std::fmt::Binary trait。在这里如果使用的时默认语法{},则会调用std::fmt::Display trait。如果使用问好语法{:?}则会调用std::fmt::Debug trait
-
-    let b: f32 = unsafe {
-        std::mem::transmute(frankentype)
-    };
-    println!("{}", b);
-    assert_eq!(a, b);                        // <4>
+    let (a, b) = (200, 200);
+    let c: u8 = a + b; // 如果没有这个类型声明，rust不会假设你想要创建一种不可能的情况
+    println!("200 + 200 = {}", c);
 }
