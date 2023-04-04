8fe607a932fa86263ea3138e5bdc6a75bd2d2e29
cisco <codingmaster.wang@outlook.com>
2023-04-04 22:48:45 +0800

    浮点数

--- dev/src/main.rs
+++ dev/src/main.rs
@@ -1,11 +1,10 @@
-use std::mem::transmute;
-
 fn main() {
-    let big_endian: [u8; 4] = [0xAA, 0xBB, 0xCC, 0xDD];
-    let little_endian: [u8; 4] = [0xDD, 0xCC, 0xBB, 0xAA];
+    // 分离出指数，从一个f32中分离并解析出指数
+    let n: f32 = 42.42;
+    let n_bits: u32 = n.to_bits(); // 把f32的位模式解释为一个u32，以便执行后面操作
+    let exponent_ = n_bits >> 23; // 把指数部分的8的位数据位执行右移位，覆盖尾数部分
+    let exponent_ = exponent_ & 0xff; // 使用与掩码操作过滤符号位。只有最右边的8位位数据被保留下来
+    let exponent = (exponent_ as i32) - 127; // 把保留下来的位数据解释成一个有符号整数，然后依据标准定义。还需要减去指数偏置量。
 
-    let a: i32 = unsafe { transmute(big_endian) };
-    let b: i32 = unsafe { transmute(little_endian) };
-
-    println!("{} vs {}", a, b);
+    println!("{} vs {}", n_bits, exponent);
 }
