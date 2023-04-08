56e80646d0f2e2b33cf496590dd433f3feea9bd2
cisco <codingmaster.wang@outlook.com>
2023-04-08 18:01:29 +0800

    堆栈

--- dev/src/main.rs
+++ dev/src/main.rs
@@ -1,73 +1,10 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct Q7(i8);
-
-impl From<f64> for Q7 {
-    fn from(n: f64) -> Self {
-        if n >= 1.0 {
-            Q7(127)
-        } else if n <= -1.0 {
-            Q7(-128)
-        } else {
-            Q7((n * 128.0) as i8)
-        }
-    }
+fn main() {
+    let a = Box::new(1); // 在堆上分配内存， a 为指向整数的引用，
+    let b = Box::new(1);
+    let c = Box::new(1);
+    let result1 = *a + *b + *c;
+    drop(a); // drop() 用于在对象所在的作用域结束之前删除对象，实现了Drop的类型，就会有一个drop方法，但是在用户代码中显式调用它是不合法的。而使用std::mem::drop则可以绕过这个规则
+    let d = Box::new(1);
+    let result2 = *b + *c + *d;
+    println!("{} {}", result1, result2)
 }
-
-impl From<Q7> for f64 {
-    fn from(n: Q7) -> f64 {
-        (n.0 as f64) * 2f64.powf(-7.0)
-    }
-}
-
-impl From<f32> for Q7 {
-    fn from(n: f32) -> Self {
-        Q7::from(n as f64)
-    }
-}
-
-impl From<Q7> for f32 {
-    fn from(n: Q7) -> f32 {
-        f64::from(n) as f32
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    // 在此文件中定义了一个子模块
-    use super::*;    // 把父模块中的元素导入此子模块的局部作用域中。那些被标记为pub的语法项，在这里都是可以访问到的
-
-    #[test]
-    fn out_of_bounds() {
-        assert_eq!(Q7::from(10.), Q7::from(1.));
-        assert_eq!(Q7::from(-10.), Q7::from(-1.));
-    }
-
-    #[test]
-    fn f32_to_q7() {
-        let n1: f32 = 0.7;
-        let q1 = Q7::from(n1);
-
-        let n2 = -0.4;
-        let q2 = Q7::from(n2);
-
-        let n3 = 123.0;
-        let q3 = Q7::from(n3);
-
-        assert_eq!(q1, Q7(89));
-        assert_eq!(q2, Q7(-51));
-        assert_eq!(q3, Q7(127));
-    }
-
-    #[test]
-    fn q7_to_f32() {
-        let q1 = Q7::from(0.7);
-        let n1 = f32::from(q1);
-        assert_eq!(n1, 0.6953125);
-
-        let q2 = Q7::from(n1);
-        let n2 = f32::from(q2);
-        assert_eq!(n1, n2);
-    }
-}
-
-fn main() {}
